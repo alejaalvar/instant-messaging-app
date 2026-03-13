372e27a64f79c10045a5177ada10a7e0115d1799
Alejandro Alvarado <alejalva@pdx.edu>
2026-03-12 19:42:44 -0700

    Provide explantory header docs for the auth middleware's role in the system

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -1,14 +1,27 @@
+/**
+ * @file AuthMiddleware.js
+ * @author Alejandro Alvarado
+ * @brief JWT authentication middleware.
+ *
+ * @description
+ * Create the middleware that verifies the JWT token
+ * from the user. If the JWT is expired, it returns an
+ * error. If it is not there, it returns an error. If
+ * verification passes, control is passed to the next
+ * function.
+ */
+
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.jwt; // Access the cookie you set in signup/login
-  
+
   if (!token) return res.status(401).send("You are not authenticated!");
 
   jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
     if (err) return res.status(403).send("Token is not valid!");
-    
+
     req.userId = payload.userId; // Extract the ID we embedded in the token
-    next(); // Pass control to the update-profile logic
+    next();
   });
 };
